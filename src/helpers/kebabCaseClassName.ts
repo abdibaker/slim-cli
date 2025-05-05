2bf4593cbe94a76d460a53a0befdf14290bfe761
Abdillah Bakari <abdibaker1@gmail.com>
2025-05-05 15:58:18 +0300

    Add pluralization exception for shehia and add debug log for className

--- src/helpers/kebabCaseClassName.ts
+++ src/helpers/kebabCaseClassName.ts
@@ -4,7 +4,11 @@ export function kebabCaseClassName(className: string): string {
   const words = inflection.underscore(className).split('_');
   const lastIndex = words.length - 1;
 
-  if (words[lastIndex] && words[lastIndex] !== 'auth') {
+  if (
+    words[lastIndex] &&
+    words[lastIndex] !== 'auth' &&
+    words[lastIndex] !== 'shehia'
+  ) {
     words[lastIndex] = inflection.pluralize(words[lastIndex]);
   }
 
